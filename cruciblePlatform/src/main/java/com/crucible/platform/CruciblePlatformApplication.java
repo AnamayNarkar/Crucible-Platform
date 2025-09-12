345911e2f3eb411b6d9f76c2e0a5060ad8c01baa
Anamay Narkar <anamay.narkar.102@gmail.com>
2025-09-12 22:07:55 +0530

    rename frontend && backend init w/ dependencies

---
+++ cruciblePlatform/src/main/java/com/crucible/platform/CruciblePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.crucible.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruciblePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruciblePlatformApplication.class, args);
+	}
+
+}
---
+++ cruciblePlatform/src/test/java/com/crucible/platform/CruciblePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crucible.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruciblePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
